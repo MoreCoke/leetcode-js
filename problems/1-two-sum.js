46e082d1aa45672bd3d6df7a32c8651ea6ad9f52
MoreCoke <seed45699@gmail.com>
2022-06-17 22:24:00 +0800

    Update 1-two-sum

--- problems/1-two-sum.js
+++ problems/1-two-sum.js
@@ -3,21 +3,30 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-  const numsMap = new Map(
-      nums.map((num, idx) => [num, idx])
-  );
+var twoSum = function (nums, target) {
+  // 建立 Map 加快拿資料的速度
+  // 加入 if 避免拿到重複的元素
+  // 利用 target - num 來找出 num 的互補數
+
+  const numsMap = new Map(nums.map((num, idx) => [num, idx]));
   let ans = [];
   nums.some((num, idx) => {
-      const rest = target - num;
-      if (numsMap.has(rest) && idx !==numsMap.get(rest)) {
-          ans = [idx, numsMap.get(rest)];
-          return true;
-      }
-  })
+    const rest = target - num;
+    if (numsMap.has(rest) && idx !== numsMap.get(rest)) {
+      ans = [idx, numsMap.get(rest)];
+      return true;
+    }
+  });
   return ans;
 };
 
-// 建立 Map 加快拿資料的速度
-// 加入 if 避免拿到重複的元素
-// 利用 target - num 來找出 num 的互補數
+var twoSum = function (nums, target) {
+  const obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    const rest = target - nums[i];
+    if (obj.hasOwnProperty(rest)) {
+      return [obj[rest], i];
+    }
+    obj[nums[i]] = i;
+  }
+};
