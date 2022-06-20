e7e1afa7541f31cfc7b87da97a551dc19768d072
MoreCoke <seed45699@gmail.com>
2022-06-20 17:22:25 +0800

    Update 13

--- problems/13-roman-to-integer.js
+++ problems/13-roman-to-integer.js
@@ -24,3 +24,29 @@ var romanToInt = function (s) {
 };
 
 // https://zyrastory.com/en/coding-en/leetcode-en/leetcode-13-roman-to-integer-solution-and-explanation-en/
+
+var romanToInt = function (s) {
+  const obj = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  const arr = s.split('');
+  let ans = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const current = obj[arr[i]];
+    const next = obj[arr[i + 1]];
+    // IV XL 的共通性是當前數字比前一個數字大
+    if (next > current) {
+      ans += next - current;
+      i++; // 跳過下一次的迴圈
+    } else {
+      ans += current;
+    }
+  }
+  return ans;
+};
