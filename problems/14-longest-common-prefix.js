7531236dcccb6d0ec04e721c409c0b0a6f3d698f
MoreCoke <seed45699@gmail.com>
2022-06-23 00:53:20 +0800

    Update 14

--- problems/14-longest-common-prefix.js
+++ problems/14-longest-common-prefix.js
@@ -39,3 +39,37 @@ var longestCommonPrefix = function (strs) {
   }
   return arr[0];
 };
+
+// 3rd
+// 1. 用 strs[0] 當基準，和陣列中的其他元素進行比較
+// 2. strs 用 for 跑迴圈看每個元素中同個 index 的情況下字元是否相同，使用 currentIdx 紀錄現在執行到哪個 index
+// 3. 不知道用 strs 的迴圈要跑幾次，外面包一個 while 處理
+// 4. 因為用 strs[0] 當基準，在 strs 的迴圈從 1 開始跑
+// 5. 加入 break 處理，如果當前元素中的字元和 strs[0] 對不上就中斷 for 迴圈和 while 迴圈
+// 6. 如果 for 迴圈順利跑完代表當前 str[0][currentIdx] 這個字元是所有元素都有的，把這個字元加入 ans，currentIdx + 1 去跑下一次的迴圈
+// ----
+// 7. 沒考慮到 strs 只有一個元素的情形，所以在程式執行一開始加入針對一個元素的判斷
+// 8. 沒考慮到 strs 所有元素都相同造成無窮迴圈的情況，利用 currentIdx++ 可能會超過 strs[0] 的情況來中斷 while 迴圈
+var longestCommonPrefix = function (strs) {
+  if (strs.length === 1) {
+    return strs[0];
+  }
+  let ans = '';
+  let currentIdx = 0;
+  let isFinished = false;
+  while (true) {
+    for (let i = 1; i < strs.length; i++) {
+      if (strs[0][currentIdx] !== strs[i][currentIdx]) {
+        isFinished = true;
+        break;
+      }
+    }
+    if (isFinished || currentIdx > strs[0].length - 1) {
+      break;
+    } else {
+      ans += strs[0][currentIdx];
+      currentIdx++;
+    }
+  }
+  return ans;
+};
